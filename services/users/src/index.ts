e50855964bff58f21c6c41713e6ffc9d34e44b43
Nick Wylynko <nick@blueskylabs.com.au>
2022-05-11 15:21:16 +0800

    Add fastify to users service

--- services/users/src/index.ts
+++ services/users/src/index.ts
@@ -1,9 +1,20 @@
-
+import Fastify from "fastify";
 import { hello } from "@bakery/hello";
 
-const main = async () => {
+const app = Fastify({ logger: true });
+
+app.get("/", async () => {
   const message = await hello();
-  console.log(message);
+  return { message };
+})
+
+const main = async () => {
+  try {
+    await app.listen("4000", "0.0.0.0")
+    console.log(`Server listening at`, app.server.address())
+  } catch (error) {
+    console.error(`failed to start server: ${error}`)
+  }
 }
 
 main()
